445c18fdded2c83337aa686fea45882bc89bf1f2
Emily Shurtz <emily@Emilys-MacBook-Pro.local>
2022-02-18 00:39:15 -0700

    fixed bugs

--- script.js
+++ script.js
@@ -8,17 +8,20 @@ var leadEl=document.querySelector('lead');
 var now = moment().format("ddd, MMM Do YYYY");
 $("#currentDay").text(now);
 
-var currentTime= moment().format("h:mm")
-var block=document.querySelectorAll('#hour')
-
-if (block<currentTime){
-$(".container").addClass(".past")
-}else if (block=currentTime) {
-    $(".container").addClass(".present");
-}else {block>currentTime,
-    $(".container").addClass(".future");
-};
-
+var currentTime= moment().hours()
+$(".time-block").each(function(){
+    var block=parseInt($(this).attr('id'))
+    console.log(block)
+    console.log(currentTime)
+    if (block<currentTime){
+        $(this).addClass("past")
+    }else if (block===currentTime) {
+        $(this).addClass("present");
+    }else{
+        $(this).addClass("future");
+    };
+    
+})
 
 // save button-- save in local storage
 function saveData(){
@@ -31,13 +34,13 @@ function saveData(){
 
 //  refresh-saved-- items shown
 
-$("#hour-09").val(localStorage.getItem("#hour-09"));
-$("#hour-10").val(localStorage.getItem("#hour-10"));
-$("#hour-11").val(localStorage.getItem("#hour-11"));
-$("#hour-12").val(localStorage.getItem("#hour-12"));
-$("#hour-13").val(localStorage.getItem("#hour-13"));
-$("#hour-14").val(localStorage.getItem("#hour-14"));
-$("#hour-15").val(localStorage.getItem("#hour-15"));
-$("#hour-16").val(localStorage.getItem("#hour-16"));
-$("#hour-17").val(localStorage.getItem("#hour-17"));
-$("#hour-18").val(localStorage.getItem("#hour-18"));
+$("#hour-09").siblings(".col-6").children().val(localStorage.getItem("hour-09"));
+$("#hour-10").siblings(".col-6").children().val(localStorage.getItem("hour-10"));
+$("#hour-11").siblings(".col-6").children().val(localStorage.getItem("hour-11"));
+$("#hour-12").siblings(".col-6").children().val(localStorage.getItem("hour-12"));
+$("#hour-13").siblings(".col-6").children().val(localStorage.getItem("hour-13"));
+$("#hour-14").siblings(".col-6").children().val(localStorage.getItem("hour-14"));
+$("#hour-15").siblings(".col-6").children().val(localStorage.getItem("hour-15"));
+$("#hour-16").siblings(".col-6").children().val(localStorage.getItem("hour-16"));
+$("#hour-17").siblings(".col-6").children().val(localStorage.getItem("hour-17"));
+$("#hour-18").siblings(".col-6").children().val(localStorage.getItem("hour-18"));
